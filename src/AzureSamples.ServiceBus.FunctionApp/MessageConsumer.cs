550a9cbca1bd1a2a8d2e878d57b543006890c593
Hossein Nassiri <hosseinnassiri2002@hotmail.com>
2023-12-03 19:50:08 -0500

    configured log level

--- src/AzureSamples.ServiceBus.FunctionApp/MessageConsumer.cs
+++ src/AzureSamples.ServiceBus.FunctionApp/MessageConsumer.cs
@@ -16,14 +16,14 @@ public class MessageConsumer
         _logger = logger;
         _configuration = configuration;
 
-        // Read configuration data from app config
+        // read configuration data from app config
         string keyName = "myKey";
         string keyValue = _configuration[keyName];
         _logger.LogInformation("Reading value from app configuration. {key}: {value}", keyName, keyValue);
     }
 
     [Function(nameof(MessageConsumer))]
-    [BlobOutput("archive/{name}-output.json", Connection = "ArchiveBlobConnection")]
+    [BlobOutput("archive/{name}-{datetime:yyyyMMdd HHmmss}-output.json", Connection = "ArchiveBlobConnection")]
 	public SampleEvent Run([ServiceBusTrigger("%ServiceBusQueue%", Connection = "ServiceBusConnection")] SampleEvent message,
 				   FunctionContext context,
 				   CancellationToken cancellationToken)
@@ -32,14 +32,13 @@ public class MessageConsumer
 		if (cancellationToken.IsCancellationRequested)
 		{
 			_logger.LogInformation("A cancellation token was received, taking precautionary actions.");
-			// Take precautions like noting how far along you are with processing the batch
+			// take precautions like noting how far along you are with processing the batch
 			_logger.LogInformation("Precautionary activities complete.");
 		}
 
 		_logger.LogInformation("Message Body: {message}", JsonSerializer.Serialize(message));
-        _logger.LogWarning("Message Body Warning: {message}", JsonSerializer.Serialize(message));
 
-		// Blob Output
+		// blob output
 		return message;
 	}
 }
